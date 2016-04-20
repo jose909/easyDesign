f4a910641f8990f94c4f7653593e1e43b0bfe36f
jose909 <rosaleayal19@hotmail.com>
2016-04-19 20:14:01 -0600

    It has been added the gem OmniAuth-twitter.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,14 @@ module ApplicationHelper
 			session[:omniauth_data][:email]
 		else
 			""
-			
 		end
-		
+	end
+
+	def get_username_oauth
+			if session[:omniauth_data]
+				session[:omniauth_data][:username]
+			else
+				""	
+			end
 	end
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -4,22 +4,35 @@ class Usuario < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  devise :omniauthable, omniauth_providers: [:facebook]
+  devise :omniauthable, omniauth_providers: [:facebook, :twitter]
+
+  
+
+	#validate :validacion_personalizada, on: :create
 
   def self.find_or_create_by_omniauth(auth)
   	usuario = Usuario.where(provider: auth[:provider], uid: auth[:uid]).first
 
 	  	unless usuario
 	  		usuario = Usuario.create(
-	  			nombre: auth[:nombre],
-	  			apellido: auth[:apellido],
-	  			username: auth[:username],
-	  			email: auth[:email],
-	  			uid: auth[:uid],
-	  			provider: auth[:provider],
-	  			password: Devise.friendly_token[0,20]
-	  			)
-	  	end
-     	
-     end   
+										  			nombre: auth[:nombre],
+										  			apellido: auth[:apellido],
+										  			username: auth[:username],
+										  			email: auth[:email],
+										  			uid: auth[:uid],
+										  			provider: auth[:provider],
+										  			password: Devise.friendly_token[0,20])
+	  	end     	
+  end 
+
+  # private
+
+  #    def validacion_personalizada
+  #      if true
+
+  #      else
+  #        errors.add(:username, "Tu username no es valido")
+
+  #      end
+  #    end  
 end
