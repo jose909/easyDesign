c6c133231b8e8e2f4ecb9100f674cfd3cc486c40
jose909 <rosaleayal19@hotmail.com>
2016-05-07 11:44:09 -0600

    add a cost field and change the page layout and page post show

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ActiveRecord::Base
-  belongs_to :usuario
+  belongs_to :usuario, dependent: :destroy
   has_many :attachments
   validates :titulo, presence: true, uniqueness: true
+  before_save :valores_por_default
   include Picturable
   include PublicActivity::Model
   tracked owner: Proc.new{ |controller,model| controller.current_usuario }
  
+ def valores_por_default
+ 	self.costo ||= 0
+ 	
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160507154954) do
+ActiveRecord::Schema.define(version: 20160507165755) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id"
@@ -55,8 +55,9 @@ ActiveRecord::Schema.define(version: 20160507154954) do
     t.text     "contenido"
     t.string   "extension"
     t.integer  "usuario_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.decimal  "costo",      precision: 10, scale: 3
   end
 
   add_index "posts", ["usuario_id"], name: "index_posts_on_usuario_id"
